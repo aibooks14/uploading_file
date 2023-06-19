7a3330baec55c3363d22b4289ff117de977349fe
aibooks14 <101002119+aibooks14@users.noreply.github.com>
2023-06-20 07:05:37 +0900

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -35,3 +35,46 @@ public class UserController {
         return "redirect:/user";
     }
 }
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user")
+    public String showUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user-form";
+    }
+
+    @PostMapping("/user")
+    public String saveUser(@ModelAttribute User user, @RequestParam("file") MultipartFile file) throws IOException {
+        // Save the user to the database
+        userRepository.save(user);
+
+        // Process the uploaded file
+        if (!file.isEmpty()) {
+            // Save the file or perform further processing
+        }
+
+        return "redirect:/user";
+    }
+}
+
